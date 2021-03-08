4861596d8759367acd2b6b7051b19a85df6b2f62
Pavan <Pavan@192.168.0.7>
2021-03-08 21:11:38 +0530

    MyFirstcommit

---
+++ appdemo/src/main/java/appdemo/Devops.java
@@ -0,0 +1,17 @@
+package appdemo;
+
+public class Devops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year=2024;    
+	    if(((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0)){  
+	        System.out.println("LEAP YEAR");  
+	    }  
+	    else{  
+	        System.out.println("COMMON YEAR");  
+	    }  
+	}
+	}
+
+
